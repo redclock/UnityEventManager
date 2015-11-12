5d85dd76df92c58955143cc9394e95d2bdd3252c
chunhui.yao <chunhui.yao@zentertain.net>
2015-11-12 14:51:20 +0800

    event manager class

---
+++ eventManager/Assets/Scripts/events/EventListener.cs
@@ -0,0 +1,27 @@
+﻿
+public interface EventListener {
+	System.Type eventType { get; }
+	void callDelegate(GameEvent e);
+	void markDeleted ();
+	bool isDeleted ();
+}
+
+public class EventListener<T>: EventListener where T: GameEvent {
+	public delegate void EventDelegate (T e);
+	
+	private bool _isDeleted;
+	private EventDelegate _delegate;
+	
+	public System.Type eventType { get { return typeof(T); } }
+	
+	public void markDeleted() { _isDeleted = true; }
+	public bool isDeleted() { return _isDeleted; }
+	
+	public void callDelegate(GameEvent e) {
+		_delegate.Invoke ((T)e);
+	}
+	
+	public EventListener(EventDelegate del) {
+		_delegate = del;		
+	}
+}
---
+++ eventManager/Assets/Scripts/events/EventManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+
+public class EventManager {
+	private class EventRecord {
+		public System.Type eventType;
+		public bool isDispactching = false;
+		public List<EventListener> listeners = new List<EventListener>();
+
+		public EventRecord(System.Type eventType) {
+			this.eventType = eventType;
+		}
+
+		public void addListener(EventListener listener) {
+			listeners.Add (listener);
+		}
+
+		public void dispatch(GameEvent gameEvent) {
+			isDispactching = true;
+			for (int i = listeners.Count; i >= 0; i--) {
+				var listener = listeners[i];
+				if (!listener.isDeleted()) {
+					listener.callDelegate(gameEvent);
+				}
+			}
+			isDispactching = false;
+
+			for (int i = listeners.Count; i >= 0; i--) {
+				var listener = listeners [i];
+				if (listener.isDeleted ()) {
+					listeners.RemoveAt(i);
+				}
+			}
+		}
+
+		public void removeListener(EventListener listener) {
+			listener.markDeleted ();
+		}
+
+		public void clear() {
+			for (int i = listeners.Count; i >= 0; i--) {
+				listeners [i].markDeleted();
+			}
+			listeners.Clear ();
+		}
+	}
+
+	private Dictionary<System.Type, EventRecord> _records = new Dictionary<System.Type, EventRecord>();
+
+	private EventRecord getEventRecord(System.Type eventType) {
+		EventRecord record;
+		if (!_records.TryGetValue (eventType, out record)) {
+			record = new EventRecord(eventType);
+			_records.Add(eventType, record);
+		}
+		return record;
+	}
+
+	public EventListener listen<T>(EventListener<T>.EventDelegate callback) where T : GameEvent {
+		EventListener listener = new EventListener<T> (callback);
+		getEventRecord (typeof(T)).addListener (listener);
+		return listener;
+	}
+
+	public void removeListener<T>(EventListener listener) {
+		getEventRecord (listener.eventType).removeListener (listener);
+	}
+
+	public void send(GameEvent gameEvent) {
+		getEventRecord (gameEvent.GetType ()).dispatch (gameEvent);
+	}
+
+	public void removeEventListeners(GameEvent gameEvent) {
+		getEventRecord (gameEvent.GetType ()).clear ();
+	}
+}

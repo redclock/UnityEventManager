fcb00ba05e0ccd3acf685b5a2482b02c25088a73
chunhui.yao <chunhui.yao@zentertain.net>
2015-11-20 19:07:17 +0800

    game logic

---
+++ eventManager/Assets/Scripts/common/component/GameLogicBase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogicBase : MonoBehaviour {
+
+	private GameEventComponent _eventComp;
+	protected GameEventComponent eventComp {
+		get {
+			if (_eventComp == null)
+				_eventComp = GameEventComponent.Create(gameObject);
+			return _eventComp;
+		}
+	}
+
+}
---
+++ eventManager/Assets/Scripts/common/component/GameRootBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameRootBase : MonoBehaviour {
+
+	public static GameObject RootObject;
+
+	protected virtual void Awake() {
+		RootObject = gameObject;
+	}
+}
